b2a26fd7c9a38fb6b362901d82fa471ead7819f6
Guillermo Gutierrez <guillerg@gmail.com>
2017-11-11 13:06:08 +0100

    Quiz views

--- components/QuizView.js
+++ components/QuizView.js
@@ -13,8 +13,47 @@ class QuizView extends Component {
     showAnswer: false
   };
 
-  render(){
+  correctAnswer = () =>{
+    this.setState(({ correctAnswers }) => ({
+     correctAnswers: correctAnswers + 1
+   }));
+   this.nextQuestion();
+  }
 
+  nextQuestion = () => {
+    this.setState(({ currentQuestion }) => ({
+      currentQuestion: currentQuestion + 1,
+      showAnswer: false
+    }));
+  };
+
+  toggleCard = () => {
+      this.setState(({ showAnswer }) => ({ showAnswer: !showAnswer }));
+    };
+
+  return = () => {
+      this.props.navigation.goBack();
+    }
+
+  render(){
+    const { questions } = this.props;
+    const { currentQuestion, correctQuestion, showAnswer } = this.state;
+
+    return(
+      <View style={{ flex: 1 }}>
+        {currentQuestion < questions.length ? (
+          <View >
+
+          </View>
+
+        ) : (
+          <View >
+
+          </View>
+        )}
+      </View>
+
+    )
   }
 }
 
